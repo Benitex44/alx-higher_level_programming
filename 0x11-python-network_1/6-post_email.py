97c26fe93903eda37b94e05bb6c695b7be14f97c
Benitex44 <benbenedict971@gmail.com>
2022-09-24 00:02:46 +0100

    Python script that takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter,

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""Check status"""
-import requests
-import sys
-
-
-def post():
-    """status"""
-    result = requests.post(sys.argv[1], data={"email": sys.argv[2]})
-
-    print(result.text)
-
-if __name__ == "__main__":
-    post()
+"""
+takes in a URL and an email address, sends a POST request to the passed URL
+with the email as a parameter, and finally displays the body of the response
+"""
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    payload = {'email': argv[2]}
+    r = requests.post(argv[1], data=payload)
+    print(r.text)
