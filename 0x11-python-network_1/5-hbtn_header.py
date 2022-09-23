dd4ee736cfad08817cdb843a2591f0cf151402c4
Benitex44 <benbenedict971@gmail.com>
2022-09-23 23:59:33 +0100

    Python script that takes in a URL, sends a request to the URL and displays the value of the variable

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""Check status"""
-import requests
-import sys
-
-
-def header():
-    """status"""
-    result = requests.get(sys.argv[1])
-
-    print(result.headers.get("X-Request-Id", None))
-
-if __name__ == "__main__":
-    header()
+"""
+takes in a URL, sends a request to the URL and displays the value of the
+variable X-Request-Id in the response header
+"""
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    r = requests.get(argv[1])
+    print(r.headers.get('X-Request-Id'))
